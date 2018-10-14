70ae8d862742a3a9f6d75dc037df0a9d2dca0f05
Vipanshi <35163221+Vipanshi@users.noreply.github.com>
2018-10-14 22:52:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+var express=require('express');
+
+
+var app=express();
+var server=require('http').createServer(app);
+
+
+var port=3000;
+
+server.listen(port,function(){
+    console.log("server"+port);
+    
+});
+
+var io=require('socket.io').listen(server);
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+//to recieve events on server side
+io.sockets.on("connection",function(socket){   //use to make connection with socket
+   // console.log("socket connected");
+
+    socket.on("chat",function(data){
+        
+        io.sockets.emit('chat',data);
+        
+    });
+    
+    //catching typing msg
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data);
+    });
+});
+
+
+
